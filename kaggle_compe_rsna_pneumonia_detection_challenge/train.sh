b4e7ee1653bd8a389fc4caf260355153cb1b632f
Gin04gh <itoeiji110704+gin04@gmail.com>
2018-10-02 10:00:58 +0000

    add train

---
+++ kaggle_compe_rsna_pneumonia_detection_challenge/train.sh
@@ -0,0 +1,1 @@
+docker run --runtime=nvidia -it --rm -v $(pwd):/outdir kaggle_compe_rsna_pneumonia_detection_challenge python object_detection/legacy/train.py --logtostderr --train_dir=/outdir/result/train/ --pipeline_config_path=/outdir/ssd_mobilenet_v1_pets.config
